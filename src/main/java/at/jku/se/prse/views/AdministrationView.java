55708ebfc67c3e50ca1eaf19a8a53688c048c051
marti <martin.froschauer@gmail.com>
2023-11-12 17:30:31 +0100

    #4

--- src/main/java/at/jku/se/prse/views/AdministrationView.java
+++ src/main/java/at/jku/se/prse/views/AdministrationView.java
@@ -6,9 +6,12 @@ import at.jku.se.prse.services.FahrtService;
 
 import jakarta.annotation.PostConstruct;
 import jakarta.faces.annotation.View;
+import jakarta.faces.application.FacesMessage;
+import jakarta.faces.context.FacesContext;
 import lombok.Getter;
 import lombok.Setter;
 
+import org.primefaces.event.RowEditEvent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -38,5 +41,16 @@ public class AdministrationView {
     public void saveNewFahrt() {
         fahrtService.save(newFahrt);
         initFahrten();
+    }
+    public void rowEditFahrt(RowEditEvent<Fahrt> event) {
+        fahrtService.save(event.getObject());
+        FacesMessage msg = new FacesMessage("Edited", "Fahrt " + event.getObject().getId());
+        FacesContext.getCurrentInstance().addMessage(null, msg);
+        initFahrten();
+    }
+
+    public void rowCancelFahrt(RowEditEvent<Fahrt> event) {
+        FacesMessage msg = new FacesMessage("Edit Cancelled", "Fahrt " + event.getObject().getId());
+        FacesContext.getCurrentInstance().addMessage(null, msg);
     }
 }
