6f2ad944b73905bf7ebdeed6f3613fa5783f3ffa
alinemavie <a.sageder111@gmail.com>
2023-12-01 17:48:24 +0100

    #36 added backend methods to frontend

---
+++ src/main/java/com/example/fahrtenbuch/business/VehicleFacade.java
@@ -0,0 +1,86 @@
+package com.example.fahrtenbuch.business;
+import com.example.fahrtenbuch.entities.Vehicle;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class VehicleFacade {
+    private Connection conn;
+    public VehicleFacade() {
+        DatabaseConnection databaseConnection = new DatabaseConnection();
+        conn = databaseConnection.getConnection();
+    }
+
+    public Vehicle getVehicleById(Integer id) throws SQLException {
+        Vehicle vehicle = null;
+        String query = "SELECT * FROM vehicle WHERE vehicle_id = ?";
+
+        try {
+            PreparedStatement preparedStatement = conn.prepareStatement(query);
+            preparedStatement.setInt(1, id);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            if (resultSet.next()) {
+                vehicle = new Vehicle();
+                vehicle.setVehicleId(resultSet.getInt("vehicle_id"));
+                vehicle.setLicensePlate(resultSet.getString("license_plate"));
+                vehicle.setOdometer(resultSet.getDouble("odometer"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return vehicle;
+    }
+
+    public List<Vehicle> getAllVehicles() {
+        List<Vehicle> vehicles = new ArrayList<>();
+        String query = "SELECT * FROM vehicle";
+
+        try {
+            PreparedStatement preparedStatement = conn.prepareStatement(query);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                Vehicle vehicle = new Vehicle();
+                vehicle.setVehicleId(resultSet.getInt("vehicle_id"));
+                vehicle.setLicensePlate(resultSet.getString("license_plate"));
+                vehicle.setOdometer(resultSet.getDouble("odometer"));
+                vehicles.add(vehicle);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return vehicles;
+    }
+
+    public void persistVehicle(Vehicle v) {
+        String query = "INSERT INTO vehicle (license_plate, odometer) VALUES (?, ?)";
+
+        try {
+            PreparedStatement preparedStatement = conn.prepareStatement(query);
+            preparedStatement.setString(1, v.getLicensePlate());
+            preparedStatement.setDouble(2, v.getOdometer());
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void deleteVehicleById(Integer id) {
+        String query = "DELETE FROM vehicle WHERE vehicle_id = ?";
+
+        try {
+            PreparedStatement preparedStatement = conn.prepareStatement(query);
+            preparedStatement.setInt(1, id);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/com/example/fahrtenbuch/entities/Category.java
@@ -0,0 +1,35 @@
+package com.example.fahrtenbuch.entities;
+
+public class Category {
+    private Integer category_id;
+    private String name;
+
+    public Category(Integer category_id, String name) {
+        this.category_id = category_id;
+        this.name = name;
+    }
+
+    public Category() {
+
+    }
+
+    public Category(String name) {
+        this.name = name;
+    }
+
+    public Integer getCategory_id() {
+        return category_id;
+    }
+
+    public void setCategory_id(Integer category_id) {
+        this.category_id = category_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
