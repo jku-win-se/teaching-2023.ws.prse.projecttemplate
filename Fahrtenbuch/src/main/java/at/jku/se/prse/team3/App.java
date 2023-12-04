a5e70903bc3cc9bbc5b948891bceb88d11395e0c
HannesMyrhfr <hannesmayrhofer@gmx.net>
2023-12-04 19:38:05 +0100

    removed Console Print #PMD

--- Fahrtenbuch/src/main/java/at/jku/se/prse/team3/App.java
+++ Fahrtenbuch/src/main/java/at/jku/se/prse/team3/App.java
@@ -13,11 +13,7 @@ import java.util.List;
 
 public abstract class App {
     public static void main(String[] args) throws IOException, CsvValidationException, InterruptedException {
-        System.out.println("  ___     _        _            _            _   ");
-        System.out.println(" | __|_ _| |_  _ _| |_ ___ _ _ | |__ _  _ __| |_  ");
-        System.out.println(" | _/ _` | ' \\| '_|  _/ -_) ' \\| '_ \\ || / _| ' \\ ");
-        System.out.println(" |_|\\__,_|_||_|_|  \\__\\___|_||_|_.__/\\_,_\\__|_||_|");
-        System.out.println("         by  TEAM 3 - JKU   release 0.0.3                   ");
+
 
 
 
