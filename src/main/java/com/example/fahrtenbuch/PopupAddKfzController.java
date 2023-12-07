38816422d7c978167d461058e848c0d6551e5eeb
suchanp <philipp.su@outlook.com>
2023-12-07 22:31:26 +0100

    Fahrzeug anlegen, Frontend und Backend verknüpft

--- src/main/java/com/example/fahrtenbuch/PopupAddKfzController.java
+++ src/main/java/com/example/fahrtenbuch/PopupAddKfzController.java
@@ -1,14 +1,53 @@
 package com.example.fahrtenbuch;
 
+import com.example.fahrtenbuch.business.VehicleFacade;
+import com.example.fahrtenbuch.entities.Vehicle;
 import javafx.event.ActionEvent;
-
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class PopupAddKfzController {
-    public void addNewKfz(ActionEvent actionEvent) {
-        //TODO
+
+    @FXML
+    public TextField newKfzTextField;
+    @FXML
+    public TextField newOdometer;
+
+    private VehicleFacade vehicleFacade;
+
+    public PopupAddKfzController() {
+        // Hier kannst du auch eine Dependency Injection verwenden,
+        // um die VehicleFacade-Instanz zu erhalten.
+        this.vehicleFacade = new VehicleFacade();
     }
 
-    public void closePopup(ActionEvent actionEvent) {
-        //TODO
+    @FXML
+    public void addNewKfz(ActionEvent actionEvent) {
+        String licensePlate = newKfzTextField.getText();
+        double odometer = Double.parseDouble(newOdometer.getText());
+
+        // Erstelle ein neues Fahrzeugobjekt
+        Vehicle newVehicle = new Vehicle();
+        newVehicle.setLicensePlate(licensePlate);
+        newVehicle.setOdometer(odometer);
+
+        // Rufe persistVehicle auf, um das Fahrzeug in der Datenbank zu speichern
+        vehicleFacade.persistVehicle(newVehicle);
+
+        // Optional: Aktualisiere die Anzeige oder führe andere Aktionen durch
+        // ...
+
+        // Schließe das Popup
+        closePopup(actionEvent);
     }
+
+    @FXML
+    public void closePopup(ActionEvent actionEvent) {
+        // Schließe das Popup-Fenster
+        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        stage.close();
+    }
+
 }
