90a4b84256db9900c3fc3eee6cdf63d07ad1d7c9
agmilan <147155157+agmilan@users.noreply.github.com>
2024-01-13 18:09:27 +0100

    1- Created Cloudbackup class to upload files to Dropbox. 2- Added UI button to settings to sync persistent data files to the cloud

--- Fahrtenbuch/src/main/java/at/jku/se/prse/team3/CloudBackup.java
+++ Fahrtenbuch/src/main/java/at/jku/se/prse/team3/CloudBackup.java
@@ -17,7 +17,7 @@ public class CloudBackup {
 
     //Link to view Backup --> https://www.dropbox.com/scl/fo/3qpbm5kf5rfv9r7jh21k3/h?rlkey=litwwve441xfj954njoforq6n&dl=0
 
-    private static final String ACCESS_TOKEN = "sl.BtlVoO7FAYjh-gwJSoKiofMyyuf5RPedUeOwO8Bg4494GNvIOWWtHsX_dM7vvBM9XFbUUYmj1grDStXK56Q_1AuKKcbObs8trltRuB8U1irc-g7m21l5UWOi5ADfm7LxioA2JlH-d8FFCuY";
+    private static final String ACCESS_TOKEN = "sl.BtmNnSH9o9nmtS52tnwbNHRON-XUiMl8Q3SRF-iIsMhuYtJ6QY9ScN40WnTgwjzjNcaHKGsznCdrKRX1a9MQ1OgG7gTKIjxN5k6PxgKhS5s4dP3CdDHlGXzUG6sbqSiVt7MeZiE2CgkasEU";
     private static final String CLIENT_IDENTIFIER = "wk400vxjbjtryt0";
 
     public static void uploadDB(String localPath, String cloudPath){
